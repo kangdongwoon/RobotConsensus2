a71b09fe05635a4bd6b3fe48cf8a36a9a81bc312
kangdongwoon <52377778+kangdongwoon@users.noreply.github.com>
2021-08-02 17:47:55 +0900

    Add files via upload

---
+++ controller/src/async_server.cpp
@@ -0,0 +1,238 @@
+#include <cstdlib>
+#include <iostream>
+#include <boost/bind.hpp>
+#include <boost/asio.hpp>
+#include"datatype.h"
+#include <map>
+
+#include <ros/ros.h>
+#include "nav_msgs/Odometry.h"
+
+#include <eigen3/Eigen/Eigen>
+
+#include <brl_msgs/brl_msgs.h>
+
+using boost::asio::ip::tcp;
+using namespace std;
+
+#define SESSION_NUM 3
+class Session;
+std::map<int, Session*> g_mpSessionIDs;
+static int g_exchange = 0;
+class Session
+{
+public:
+  Session(boost::asio::io_service& io_service) : m_Socket(io_service)
+  {
+    // HOLD HEADER //
+    m_uSendPacket.stData.header[0]=m_uSendPacket.stData.header[1]=m_uSendPacket.stData.header[2]=m_uSendPacket.stData.header[3]=0x00;
+  }
+  tcp::socket& socket() {
+    return m_Socket;
+  }
+  Packet_t get_Packet(){
+    return m_uPacketBuffer;
+  }
+  void set_Packet(Packet_t pk){
+    m_uSendPacket.stData.z_x = pk.stData.z_x;
+    m_uSendPacket.stData.z_y = pk.stData.z_y;
+  }
+  void recv_Packet(){
+    memset(&m_Buf, '\0', sizeof(m_Buf));
+    m_Socket.async_read_some(boost::asio::buffer(m_Buf),
+                             boost::bind(&Session::recv_handler, this,
+                             boost::asio::placeholders::error,
+                             boost::asio::placeholders::bytes_transferred));
+  }
+  void write_Packet(){
+    if(g_exchange == 1){
+      m_uSendPacket.stData.header[0]=m_uSendPacket.stData.header[1]=m_uSendPacket.stData.header[2]=m_uSendPacket.stData.header[3]=0xFE;
+    }
+    m_uSendPacket.stData.yaw = m_uSendPacket.stData.d_x = m_uSendPacket.stData.d_y = 0;
+    m_uSendPacket.stData.mode = 3;
+    m_uSendPacket.stData.check = 0;
+
+    for(int i=8; i<sizeof(Packet_t); i++){
+      m_uSendPacket.stData.check += m_uSendPacket.buffer[i];
+    }
+    boost::asio::async_write(m_Socket,
+                             boost::asio::buffer((char*)m_uSendPacket.buffer,sizeof(Packet_t)),
+                             boost::bind(&Session::write_handler, this,
+                             boost::asio::placeholders::error,
+                             boost::asio::placeholders::bytes_transferred));
+    recv_Packet();
+  }
+  void recv_handler(const boost::system::error_code& error, size_t m_readSize)
+  {
+    if (!error)
+    {
+      m_BufWriteCnt += m_readSize;
+      for(int i = 0; i<m_readSize; i++){
+          switch (m_PacketMode) {
+          case 0: // HEADER CHECK
+              if(m_Buf[i] == 0xFF){
+                  m_checkSize++;
+                  if(m_checkSize ==4){
+                      m_PacketMode = 1;
+                  }
+              }
+              else{
+                  m_checkSize = 0;
+              }
+              break;
+
+          case 1: // CHAR * 4
+              m_uPacketBuffer.buffer[m_checkSize++] = m_Buf[i];
+              if(m_checkSize == 8) {
+                  m_PacketMode = 2;
+              }
+              break;
+
+          case 2: // DATA
+              m_uPacketBuffer.buffer[m_checkSize++] = m_Buf[i];
+              m_check += m_Buf[i];
+              if(m_checkSize == sizeof(Packet_t)){
+                  if(m_check == m_uPacketBuffer.stData.check){
+                      // Packet DATA RECV CHECK //
+//                        cout<<m_uPacketBuffer.stData.posx<<","<<m_uPacketBuffer.stData.posy<<endl;
+                  }
+                  m_check = 0;
+                  m_PacketMode = 0;
+                  m_checkSize = 0;
+              }
+          }
+      }
+      write_Packet();
+    }
+    else
+    {
+      cout<<"Error :"<<error.message()<<endl;
+      delete this;
+    }
+  }
+  void write_handler(const boost::system::error_code& error,size_t readsize){
+  }
+private:
+  tcp::socket m_Socket;
+  Packet_t m_uSendPacket;
+  Packet_t m_uPacketBuffer;
+  unsigned char m_Buf[256],m_BufWriteCnt =0, m_BufReadCnt =0;
+  unsigned char m_PacketMode = 0;
+  int m_readSize =0, m_checkSize=0;
+  unsigned char m_check=0;
+};
+
+class Server
+{
+public:
+  Server(boost::asio::io_service& io_service, short port)
+    : io_service_(io_service), acceptor_(io_service, tcp::endpoint(tcp::v4(), port))
+  {
+    m_pSession = nullptr;
+    accept_session();
+  }
+  ~Server(){
+    if (m_pSession != nullptr)
+          delete m_pSession;
+  }
+  int get_session_num(){
+    return m_session_num;
+  }
+private:
+  void accept_session()
+  {
+    m_pSession = new Session(io_service_);
+
+    if(m_session_num == SESSION_NUM) g_exchange = 1;
+
+    acceptor_.async_accept(m_pSession->socket(),
+                           boost::bind(&Server::accept_handler, this, m_pSession,
+                           boost::asio::placeholders::error));
+    g_mpSessionIDs.insert(std::pair<int,Session*>(m_session_num++,m_pSession));
+    cout<<m_session_num-1<<":"<<m_pSession<<"  "<<g_mpSessionIDs[m_session_num-1]<<endl;
+  }
+  void accept_handler(Session* m_pSession, const boost::system::error_code& error)
+  {
+    if (!error) {
+      m_pSession->recv_Packet();
+    }
+    else {
+      delete m_pSession;
+    }
+    accept_session();
+  }
+  int m_session_num void= 0;
+  boost::asio::io_service& io_service_;
+  tcp::acceptor acceptor_;
+  Session* m_pSession;
+
+};
+
+int main(int argc, char* argv[])
+{
+  ros::init(argc, argv, "async_server");
+  ros::NodeHandle nh;
+  ros::Publisher Tb1_pub = nh.advertise<brl_msgs::brl_msgs>("/tb_1/odom",10);
+  ros::Publisher Tb2_pub = nh.advertise<brl_msgs::brl_msgs>("/tb_2/odom",10);
+  ros::Publisher Tb3_pub = nh.advertise<brl_msgs::brl_msgs>("/tb_3/odom",10);
+
+  ros::Rate rate(50);
+  boost::asio::io_service io_service;
+  Server s(io_service, 2222);
+
+  Packet_t pk[SESSION_NUM];
+  Eigen::MatrixXf L(SESSION_NUM, SESSION_NUM);
+  L<<1, 0, -1,
+      -1, 1, 0,
+      0, -1, 1;
+  Eigen::MatrixXf z_ix(SESSION_NUM,1);
+  Eigen::MatrixXf z_iy(SESSION_NUM,1);
+
+  brl_msgs::brl_msgs tb1_odom;
+  brl_msgs::brl_msgs tb2_odom;
+  brl_msgs::brl_msgs tb3_odom;
+  while(ros::ok()){
+    io_service.poll();
+
+    if(g_exchange == 1){
+        // GET PACKET //
+        for(int i = 0; i < s.get_session_num()-1; i++){
+          pk[i] = g_mpSessionIDs[i]->get_Packet();
+          cout<<i<<": "<<pk[i].stData.z_x<<","<<pk[i].stData.z_y<<endl;
+        }
+        // Exchange with Laplacian //
+        z_ix << pk[0].stData.z_x,pk[1].stData.z_x,pk[2].stData.z_x;
+        z_iy << pk[0].stData.z_y,pk[1].stData.z_y,pk[2].stData.z_y;
+        z_ix = L*z_ix;
+        z_iy = L*z_iy;
+
+        // Draw Turtlebot IN Gazebo //
+        tb1_odom.posx = pk[0].stData.z_x + pk[0].stData.d_x;;
+        tb1_odom.posy = pk[0].stData.z_y + pk[0].stData.d_y;;
+        tb1_odom.yaw  = pk[0].stData.yaw;
+
+        tb2_odom.posx = pk[1].stData.z_x + pk[1].stData.d_x;
+        tb2_odom.posy = pk[1].stData.z_y + pk[1].stData.d_y;
+        tb2_odom.yaw  = pk[1].stData.yaw;
+
+        tb3_odom.posx = pk[2].stData.z_x + pk[2].stData.d_x;;
+        tb3_odom.posy = pk[2].stData.z_y + pk[2].stData.d_y;;
+        tb3_odom.yaw  = pk[2].stData.yaw;
+
+        Tb1_pub.publish(tb1_odom);
+        Tb2_pub.publish(tb2_odom);
+        Tb3_pub.publish(tb3_odom);
+
+        // SET PACKET //
+        for(int i = 0; i < s.get_session_num()-1; i++){
+          pk[i].stData.z_x = z_ix(i);
+          pk[i].stData.z_y = z_iy(i);
+          g_mpSessionIDs[i]->set_Packet(pk[i]);
+        }
+    }
+    ros::spinOnce();
+    rate.sleep();
+  }
+  return 0;
+}
+
